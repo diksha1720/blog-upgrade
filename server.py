bcc3fdf48cd52fad6f888f30f3d448c64085cdbd
Diksha1720 <dikshabharti39@gmail.com>
2022-01-09 17:02:24 +0530

    added mail sending functionality

--- server.py
+++ server.py
@@ -1,7 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
+import smtplib
 
 app = Flask(__name__)
+
+my_email = "Your Email"
+password = "Your Password"
+to_address = "Your to address"
+
+
+def send_mail(msg):
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+        connection.starttls()
+
+        connection.login(user=my_email, password=password)
+        connection.sendmail(from_addr=my_email,
+                            to_addrs=to_address,
+                            msg=msg)
 
 
 @app.route('/')
@@ -20,7 +35,10 @@ def about():
     subhead = "This is what I do"
     return render_template("about.html", img=img, head=head, subhead=subhead)
 
+
 images = ["url('../static/assets/img/cactus.jpg')", "url('../static/assets/img/bored.jpg')", "url('../static/assets/img/fasting.jpg')"]
+
+
 @app.route('/post/<id>')
 def post(id):
     response = requests.get("https://api.npoint.io/4c1b65cf4e0734179b77").json()
@@ -30,12 +48,23 @@ def post(id):
     return render_template("post.html", img=img, head=head, subhead=subhead, posts=response, id=int(id))
 
 
-@app.route('/contact')
+@app.route('/contact', methods=['GET', 'POST'])
 def contact():
     img = "url('static/assets/img/contact-bg.jpg')"
-    head = "Contact Me"
     subhead = "Have questions? I have answers"
+    if request.method == 'POST':
+        send_mail(f"{request.form['name']}\n{request.form['email']}\n{request.form['phone']}\n{request.form['msg']}")
+        head = "Message sent successfully"
+    else:
+        head = "Contact Me"
     return render_template("contact.html", img=img, head=head, subhead=subhead)
+
+
+@app.route('/form-entry', methods=['POST'])
+def form():
+    if request.method == 'POST':
+        return f"{request.form['name']}\n{request.form['email']}\n{request.form['phone']}\n{request.form['msg']}"
+
 
 
 if __name__ == '__main__':
