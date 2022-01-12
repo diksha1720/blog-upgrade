25ec462fb4d5611c4ca8b4223cfbe06620036806
Diksha1720 <dikshabharti39@gmail.com>
2022-01-12 14:34:50 +0530

    added functionality to add update and delete posts on the blog

--- server.py
+++ server.py
@@ -1,12 +1,62 @@
-from flask import Flask, render_template, request
-import requests
+from flask import Flask, render_template, redirect, url_for, request
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, URL
+from flask_ckeditor import CKEditor, CKEditorField
 import smtplib
+from datetime import datetime
+import html
 
 app = Flask(__name__)
+ckeditor = CKEditor(app)
 
-my_email = "Your Email"
-password = "Your Password"
-to_address = "Your to address"
+Bootstrap(app)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+my_email = ""
+password = ""
+to_address = ""
+
+
+class BlogPost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    subtitle = db.Column(db.String(250), nullable=False)
+    date = db.Column(db.String(250), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    img_url = db.Column(db.String(250), nullable=False)
+
+
+class NewPostForm(FlaskForm):
+    title = StringField("Blog Post Title", validators=[DataRequired()])
+    subtitle = StringField("Subtitle", validators=[DataRequired()])
+    author = StringField("Your Name", validators=[DataRequired()])
+    img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
+    body = CKEditorField("Blog Content", validators=[DataRequired()])
+    submit = SubmitField("Submit Post")
+
+# images = ["url('../static/assets/img/cactus.jpg')", "url('../static/assets/img/bored.jpg')", "url('../static/assets/img/fasting.jpg')"]
+# db.create_all()
+# response = requests.get("https://api.npoint.io/4c1b65cf4e0734179b77").json()
+# i = 0
+# for post in response:
+#     new_post = BlogPost(title=post["title"],
+#                     subtitle=post["subtitle"],
+#                     date=func.current_date(),
+#                     body=post["body"],
+#                     author="Diksha",
+#                     img_url=images[i],)
+#     i += 1
+#     db.session.add(new_post)
+#     db.session.commit()
 
 
 def send_mail(msg):
@@ -21,8 +71,9 @@ def send_mail(msg):
 
 @app.route('/')
 def home():
-    response = requests.get("https://api.npoint.io/4c1b65cf4e0734179b77").json()
-    img = "url('static/assets/img/home-bg.jpg')"
+    # response = requests.get("https://api.npoint.io/4c1b65cf4e0734179b77").json()
+    response = BlogPost.query.all()[::-1]
+    img = "static/assets/img/home-bg.jpg"
     head = "Diksha's Blog"
     subhead = "Hello everyone, Welcome to my blog"
     return render_template("index.html", img=img, head=head, subhead=subhead, posts=response)
@@ -30,27 +81,25 @@ def home():
 
 @app.route('/about')
 def about():
-    img = "url('static/assets/img/about-bg.jpg')"
+    img = "static/assets/img/about-bg.jpg"
     head = "About Me"
     subhead = "This is what I do"
     return render_template("about.html", img=img, head=head, subhead=subhead)
 
 
-images = ["url('../static/assets/img/cactus.jpg')", "url('../static/assets/img/bored.jpg')", "url('../static/assets/img/fasting.jpg')"]
-
-
 @app.route('/post/<id>')
 def post(id):
-    response = requests.get("https://api.npoint.io/4c1b65cf4e0734179b77").json()
-    img = images[int(id)-1]
-    head = response[int(id)-1]["title"]
-    subhead = response[int(id)-1]["subtitle"]
-    return render_template("post.html", img=img, head=head, subhead=subhead, posts=response, id=int(id))
+    # response = requests.get("https://api.npoint.io/4c1b65cf4e0734179b77").json()
+    response = BlogPost.query.get(id)
+    img = response.img_url
+    head = response.title
+    subhead = response.subtitle
+    return render_template("post.html", img=img, head=head, subhead=subhead, post=response, id=int(id))
 
 
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
-    img = "url('static/assets/img/contact-bg.jpg')"
+    img = "static/assets/img/contact-bg.jpg"
     subhead = "Have questions? I have answers"
     if request.method == 'POST':
         send_mail(f"{request.form['name']}\n{request.form['email']}\n{request.form['phone']}\n{request.form['msg']}")
@@ -66,6 +115,56 @@ def form():
         return f"{request.form['name']}\n{request.form['email']}\n{request.form['phone']}\n{request.form['msg']}"
 
 
+@app.route('/new_post', methods=['GET','POST'])
+def new_post():
+    form = NewPostForm()
+    if form.validate_on_submit():
+        new_post = BlogPost(title=form.title.data,
+                            subtitle=form.subtitle.data,
+                            date=datetime.now().strftime("%B %d, %Y"),
+                            body=form.body.data,
+                            author=form.author.data,
+                            img_url=form.img_url.data,)
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect(url_for('home'))
+    else:
+        img = "static/assets/img/contact-bg.jpg"
+        head = "New Post"
+        subhead = "You're going to make a great blog post!"
+        return render_template('new-post.html', form=form, img=img, head=head, subhead=subhead)
 
+
+@app.route('/edit_post/<id>', methods=['GET','POST'])
+def edit_post(id):
+    post = BlogPost.query.get(id)
+    editform = NewPostForm(title=post.title,
+    subtitle=post.subtitle,
+    img_url=post.img_url,
+    author=post.author,
+    body=post.body)
+
+    if editform.validate_on_submit():
+        post.title = editform.title.data
+        post.subtitle = editform.subtitle.data
+        post.img_url = editform.img_url.data
+        post.author = editform.author.data
+        post.body = editform.body.data
+        db.session.commit()
+        return redirect(url_for('home'))
+    else:
+        img = "../static/assets/img/contact-bg.jpg"
+        head = "Edit Post"
+        subhead = "You're going to make a great blog post!"
+        return render_template('edit.html', id=id, img=img, head=head, subhead=subhead, form=editform)
+
+
+
+@app.route('/delete_post/<ide>')
+def delete_post(ide):
+    post = BlogPost.query.get(ide)
+    db.session.delete(post)
+    db.session.commit()
+    return redirect(url_for('home'))
 if __name__ == '__main__':
     app.run(debug=True)
