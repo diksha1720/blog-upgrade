e64d40c488dcbdeac3e818e67a570692da194ba9
Diksha1720 <dikshabharti39@gmail.com>
2022-01-14 17:06:51 +0530

    add user comments

--- server.py
+++ server.py
@@ -19,6 +19,8 @@ gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=Fa
 Bootstrap(app)
 app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL1",  "sqlite:///posts.db")
+# app.config['SECRET_KEY'] = "8BYkEfBA6O6donzWlSihBXox7C0sKR6b"
+# app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///posts.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
@@ -63,7 +65,7 @@ class Comment(UserMixin, db.Model):
     text = db.Column(db.Text, nullable=False)
 
 
-# db.create_all()
+db.create_all()
 
 
 def admin_only(f):
